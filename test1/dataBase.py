a9b4974e843921aab84b662eba7c4d623ba33fc2
stas_ <dromanqs@mail.ru>
2025-01-10 20:22:32 +0500

    Подключение к бд на постгрес(16)

---
+++ test1/dataBase.py
@@ -0,0 +1,35 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+from sqlalchemy.orm import declarative_base
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+class connects():
+    def __init__(self, user, password, db, host="localhost", port=5432):
+        self.user = user
+        self.password = password
+        self.db = db
+        self.host = host
+        self.port = port
+
+    def connectmethod(self):
+        url = 'postgresql://{}:{}@{}:{}/{}'
+        url = url.format(self.user, self.password, self.host, self.port, self.db)
+
+        con = sqlalchemy.create_engine(url, client_encoding="utf-8")
+
+        return con
+
+
+load_dotenv(find_dotenv())
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+db = os.getenv('DB')
+
+conn = connects(user, password, db).connectmethod()
+Base = declarative_base()
+Base.metadata.bind = conn
+Base.metadata.create_all(bind=conn)
+Dbsession = scoped_session(sessionmaker(bind=conn))
