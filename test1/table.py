e0b438ca92aaf690ffc93d6baa5320eaae827d49
stas_ <dromanqs@mail.ru>
2025-01-11 17:51:41 +0500

    Перенос из dataBase.py Base.medata.create_all(conn) и Dbsession + обертка функций для создания таблицы по переданному значению (Создается таблица с названием которое передается в функцию в переменной table_name)

--- test1/table.py
+++ test1/table.py
@@ -1,15 +1,23 @@
-from dataBase import Base
 from sqlalchemy import String, Column
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, scoped_session, sessionmaker
+from dataBase import Base, conn
+
+def create_tablename_table(table_name):
+    class Parser(Base):
+        __tablename__ = table_name
+
+        name: Mapped[str] = mapped_column(primary_key=True)
+        price: Mapped[str] = mapped_column(String(50))
+
+        def __init__(self, name, price):
+
+            self.name = name
+            self.price = price
+
+    Base.metadata.create_all(conn)
+    return Parser
+
+Dbsession = scoped_session(sessionmaker(bind=conn))
 
 
-class Parser(Base):
-    __tablename__ = "parser"
 
-    name: Mapped[str] = mapped_column(primary_key=True)
-    price: Mapped[str] = mapped_column(String(50))
-
-    def __init__(self, name, price):
-
-        self.name = name
-        self.price = price
